d4fb55be78338a0882a223b5bb7bf1c4d8249d5d
Nicholas Barrera <nickbarerra722@gmail.com>
2023-01-10 22:04:11 -0600

    update

--- lyriks_starter_code/src/redux/services/shazamCore.js
+++ lyriks_starter_code/src/redux/services/shazamCore.js
@@ -1,20 +1,19 @@
-import {buildCreateApi, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { build } from 'vite';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 export const shazamCoreApi = createApi ({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Host', '2eb6eea384msh367af004d5e39e8p111377jsnd388daacae12');
+            headers.set('X-RapidAPI-Key', '2eb6eea384msh367af004d5e39e8p111377jsnd388daacae12');
 
             return headers;
         },
 
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world'}),
+        getTopCharts: builder.query({ query: () => 'v1/charts/world'}),
     }),
 });
 
